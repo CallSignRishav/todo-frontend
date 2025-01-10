55684f8f0a7b7e19f45d56dd0b87e9521153e413
Rishav Mondal <mondalrishav25@gmail.com>
2025-01-10 21:29:33 +0530

    Update MainHeader.tsx

--- src/components/Header/MainHeader.tsx
+++ src/components/Header/MainHeader.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import LogoutBtn from "./LogoutBtn";
 import ToggleTheme from "./ToggleTheme";
 
@@ -6,9 +7,12 @@ const MainHeader = () => {
     <>
       <header className="fixed w-full border-b border-b-foreground/10 p-4 shadow-sm backdrop-blur-lg">
         <div className="mx-auto flex max-w-screen-lg items-center justify-between">
-          <div className="bg-gradient-to-tr from-blue-500 to-pink-500 bg-clip-text text-2xl font-bold tracking-[.5rem] text-transparent">
+          <Link
+            href={"/"}
+            className="bg-gradient-to-tr from-blue-500 to-pink-500 bg-clip-text text-2xl font-bold tracking-[.5rem] text-transparent"
+          >
             TODO
-          </div>
+          </Link>
 
           <div className="flex items-center gap-8">
             <ToggleTheme />
